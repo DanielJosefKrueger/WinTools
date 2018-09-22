09013027276a96d5931bb30afa4a0652d563099f
Daniel <s-dkrueg@haw-landshut.de>
2018-09-22 23:25:53 +0200

    name changed

--- src/main/java/de/djktech/observer/ProcessInformation.java
+++ src/main/java/de/djktech/observer/ProcessInformation.java
@@ -1,6 +1,6 @@
 package de.djktech.observer;
 
-public class Process {
+public class ProcessInformation {
 
 
 
@@ -11,7 +11,7 @@ public class Process {
     private final int speichernutzung;
 
 
-    public Process(String name, int pid, String sitzungsname, int sitzungsNUmmer, int speichernutzung) {
+    public ProcessInformation(String name, int pid, String sitzungsname, int sitzungsNUmmer, int speichernutzung) {
         this.name = name;
         this.pid = pid;
         this.sitzungsname = sitzungsname;
@@ -21,7 +21,7 @@ public class Process {
 
 
     public static Process fromString(String line){
-        
+
         return null; //TODO implement
     }
 
